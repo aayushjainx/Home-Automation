3f3127d929deec7b59f528ddf03bd7df521d94f2
Aayush Jain <jaayush014@gmail.com>
2021-06-18 17:29:31 +0530

    aws fix

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.use(express.static('client/build'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-if (process.env.NODE_ENV !== 'production') {
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+//if (process.env.NODE_ENV === 'production') {
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+//}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
